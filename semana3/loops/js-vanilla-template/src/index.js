534bf2f15c5a847714bec0c91417983718470893
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-07-30 15:23:55 -0300

    exercicio

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,68 @@
+//EXERCICIO 1
+//Está repetindo uma estrutura atrelada a um numero que é incrementado a cada rodada, ate chegar no valor 5.
+//será impresso: 0 1 2 3 4 (um abaixo do outro)
+
+//EXERCICIO 2
+//a
+//19, 21, 23, 25, 27 30 (um abaixo do outro)
+
+//b
+//const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+//for (let numero of lista) {
+//  
+//	console.log(numero)
+//
+//}
+
+//DESAFIO 1
+//0,00,000,0000 (UM ABAIXO DO OUTRO)
+
+//EXERCICIO 3
+//a
+//const arrayOriginal = [2,9,7,13,23,30,1,3,54]
+//
+//for (let numero of arrayOriginal) {
+//    console.log(numero)
+//}
+
+//b
+//const arrayOriginal = [2,9,7,13,23,30,1,3,54]
+//
+//for (let numero of arrayOriginal) {
+//    console.log(numero/10)
+//}
+
+//c
+//const arrayOriginal = [2,9,7,13,23,30,1,3,54]
+//
+//for (let numero of arrayOriginal) {
+//  if (numero % 2 === 0) {
+//    let arrayPares = [numero]
+//    console.log(arrayPares)
+//  }
+//}
+
+//d
+//const arrayOriginal = [2,9,7,13,23,30,1,3,54]
+//
+//for (let i = 0; i < 9; i++) {
+//  String.arrayOriginal
+//  console.log("O elemento do index",i,"é:",arrayOriginal[i])
+//}
+
+//e
+//const arrayOriginal = [2,9,7,13,23,30,1,3,54]
+//let valorMaximo = 0
+//let valorMinimo = 10000000000
+////
+////for (let i of arrayOriginal) {
+//    if(i > valorMaximo) {
+//        valorMaximo = i
+//    } if(i < valorMinimo){
+//        valorMinimo = i
+//    }
+//}
+//console.log("O maior número é:", valorMaximo, "e o menor numero é:", valorMinimo)
+
+
+
