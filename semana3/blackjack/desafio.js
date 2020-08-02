58b3834398b720094e33855066f1662cdf92f7ac
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-02 04:22:43 -0300

    Desafio finalizado

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -1,48 +1,81 @@
 console.log("Bem vindo ao jogo de Blackjack!")
-
-let cartasU = []
-
-
-   if (confirm("Quer iniciar uma nova rodada?")){
-      const carta1 = comprarCarta();
-      const carta2 = comprarCarta();
-      const carta3 = comprarCarta();
-      const carta4 = comprarCarta();
-      let cartasU = [carta1.texto, carta2.texto]
-      let cartasC = [carta3.texto, carta4. texto]
-
-      if(cartasU === "A" || cartasC === "A"){
-         const carta1 = comprarCarta()
-         const carta2 = comprarCarta()
-         const carta3 = comprarCarta();
-         const carta4 = comprarCarta();
-         } else {
+let chave0 = true
+if(confirm("Quer iniciar uma nova rodada?")){
+   let cartasU = [comprarCarta(), comprarCarta()]
+   let cartasC = [comprarCarta(), comprarCarta()]
+   for (let i = true; i === chave0; i += i){
+      if(cartasU[0] && cartasU[1] === "A"){
+      cartasU = [comprarCarta(), comprarCarta()]
       }
-      if(confirm("Suas cartas são",carta1.texto, carta2.texto + ". A carta revelada do computador é", carta3.texto
-      +"\n"+"Deseja comprar mais uma carta?")){
-         const carta5 = comprarCarta()
-         let resultadoU = carta1.valor + carta2.valor + carta5.valor
-         if(resultadoU >= 21){
-
-         }
+      if(cartasC[0] && cartasC[1] === "A"){
+      cartasC = [comprarCarta(), comprarCarta()]
+      } 
+      else if ((cartasU[0] && cartasU[1] !== "A")&&(cartasC[0]&&cartasC[1]!=="A")){
+         chave0 = !true
       }
-      
-      let resultadoU = carta1.valor + carta2.valor
-      let resultadoC = carta3.valor + carta4.valor
-
-      console.log("Usuario - cartas:",carta1.texto, carta2.texto, "- Pontuação", resultadoU)
-      console.log("Computador - cartas:",carta3.texto, carta4.texto, "- Pontuação", resultadoC)
-
-
-      if(resultadoU > resultadoC){
-         console.log("O usuario ganhou!")
-      } else if(resultadoU < resultadoC){
-         console.log("O computador ganhou!")
-      } else{
-         console.log("Empate!")
-      }
-
-   } else {
-      console.log("O jogo acabou.")
+   }   
+   let resultadoC = 0   
+   let resultadoU = 0
+   let usu = " "
+   let pc = " "
+   for(let i of cartasU){
+      usu += i.texto + " "
    }
+   chave1 = confirm("Suas cartas são "+usu+". A carta revelada do computador é "+ cartasC[0].texto
+   +"\n"+"Deseja comprar mais uma carta?")
+   for(let i = true; i === chave1; i += i){
+      const maisUmaCarta = comprarCarta()
+      cartasU.push(maisUmaCarta)
+      resultadoU = 0
+      for(let i of cartasU){
+         resultadoU += i.valor
+      }if(resultadoU < 21){
+         usu = " "
+         for(let i of cartasU){
+            usu += i.texto + " "
+         } chave1 = confirm("Suas cartas são "+usu+". A carta revelada do computador é "+ cartasC[0].texto
+         +"\n"+"Deseja comprar mais uma carta?")
+      } else {
+         usu = " "
+         for(let i of cartasU){
+            usu += i.texto + " "
+            chave1 = !true
+         }   
+      }
+   }  
+   resultadoU = 0
+   for(let i of cartasU){
+   resultadoU += i.valor
+   }
+   for(let i of cartasC){
+   resultadoC += i.valor
+   }
+   let chave2 = resultadoU <= 21 && resultadoC <= resultadoU
+   for(let i = true; i === chave2; i += i){
+      const maisUma = comprarCarta()
+      cartasC.push(maisUma)
+      resultadoC = 0
+      for(let i of cartasC)
+      resultadoC += i.valor
+      chave2 = resultadoU <= 21 && resultadoC <= resultadoU
+   } pc = " "
+   for ( let t of cartasC){
+      pc += t.texto + " "
+   }
+   let resultadoFinal = " "
+   if((resultadoU <= 21  && resultadoU > resultadoC) || (resultadoU > 21 && resultadoU < resultadoC) || (resultadoC >= 21 && resultadoU < resultadoC)){
+      resultadoFinal += "O usuário ganhou!"
+   }
+   else if((resultadoC <= 21 && resultadoC > resultadoU) || (resultadoC > 21 && resultadoC < resultadoU) || (resultadoU >= 21 && resultadoC < resultadoU)){
+      resultadoFinal += "O computador ganhou"
+   }
+   else if(resultadoU === resultadoC){
+      resultadoFinal += "Empatou!"
+   }
+   alert("Suas cartas são " + usu + ". Sua pontuação é " + resultadoU + ".\n As cartas do computador são "
+    + pc + ". A pontuação do computador é " + resultadoC + "." + resultadoFinal)
+}else{
+   alert("O jogo acabou!")
+}
+     
 
