ee5b3a829b55e209c6066024e64a213e5f322bda
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-07 12:55:20 -0300

    bloco 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,33 @@
+//EXERCICIOS DE INTERPRETAÇÃO
+
+//1
+//Uma função criada com nome de conversorDeMoeda que tem como parametro um valor a ser armazenado, 
+//recebe uma informação do usuario em formato de numero da cotação do dolar e retorna na função o parametro multiplicado
+//pelo valor inserido pelo usuario. A variavel meuDinheiro chama a função e informa como parametro o valor 100. No console será
+//impresso o texto R$ + resultado da função que foi calculado o valor do parametro multiplicado pelo valor informado no prompt.
+
+//2
+//A função investeDinheiro tem dois parametros onde tipoDeInvestimento e valor; cria uma variavel valorAposInvestimento e entra
+// no laço switch case que envolve o parametro tipoDeInvestimento onde se poupança for true valorAposInvestimento recebe o parametro valor
+//multiplicado por 1.03; se for false verifica se Renda Fixa é true, se for valorAposInvestimento recebe o parametro valor multiplicado por 1.05;
+//se for false verifica se CDB é true, se for true valorAposInvestimento recebe o parametro valor multiplicado por 1.06; se for false verifica
+//se Ações é true, se for true valorAposInvestimento recebe o parametro valor multiplicado por 1.1; se for false o laço tem por default um alert
+//que diz que o tipo de investimento informado é incorreto. Retorna dentro da função a variavel valorAposInvestimento.
+//A variavel novoMontante invoca a função investeDinheiro com os parametros Ações e 150.
+//A variavel segundoMontante invoca a função investeDinehiro com os parametros Tesouro Direto e 200.
+//No console será impresso 165 e em uma nova linha a frase Tipo de investimento informado incorreto.
+
+//3
+//tres variaveis de array são criadas uma contendo 14 indices e as outras duas vazias.
+//O laço for.. of percorre o array preenchido e verifica se o valor de cada indice é par, se for par o valor
+//é inserido no array1, se não for par o valor é inserido no array2.
+//No console é impresso a quantidade total de indices do array, na linha de baixo a quantidade de indices do array1
+//e na linha abaixo o total de indices do array2.
+
+//4
+//Uma variavel numeros com array de 25 indices, outra variavel numero1 que recebe infinity e outra numero2 que recebe 0.
+//entra no laço for ..of que verifica se o menor valor do array é menor que a variavel numero1, se for, 
+//a variavel numero1 recebe o valor do indice do array ate encontrar o valor minimo.
+//o mesmo laço verifica se o maior numero do array é maior que o valor da variavel numero2, se for 
+//a variavel numero2 recebe o valor do indice do array ate encontrar o valor maximo.
+//No console será impresso o menor numero do array e na linha abaixo o maior.
