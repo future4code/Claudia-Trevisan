a2bae22b0e95ae497ba5bfe001faaa6c1a995af1
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-11 11:32:57 -0300

    exercicio

---
+++ labetube/labetuber/src/components/ListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function ListItem () {
+    return(
+        <nav className="menu-vertical">
+            <ul>
+                <li className="botoes-meunu-vertical">Início</li>
+                <li className="botoes-meunu-vertical">Em alta</li>
+                <li className="botoes-meunu-vertical">Inscrições</li>
+                <li className="botoes-meunu-vertical">Originais</li>
+                <li className="botoes-meunu-vertical">Histórico</li>
+            </ul>
+        </nav>
+    )
+}
