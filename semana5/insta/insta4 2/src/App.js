a654494d28c7e8c43565fd9a6caa9a9c4aaf65d1
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-13 17:11:21 -0300

    exercicios incompletos

--- semana5/insta/insta4 2/src/App.js
+++ semana5/insta/insta4 2/src/App.js
@@ -3,32 +3,48 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    post: [
+      {
+        nomeUsuario: "Paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150",
+      },
+      {
+        nomeUsuario: "Joana22",
+        fotoUsuario: "https://picsum.photos/50/55",
+        fotoPost: "https://picsum.photos/200/159",
+      },
+      {
+        nomeUsuario: "a_malukinha",
+        fotoUsuario: "https://picsum.photos/50/57",
+        fotoPost: "https://picsum.photos/200/189",
+      },
+    ]      
+  }
+
   render() {
+    const listaDeComponentes = this.state.post.map((postagem) => {
+      return (
+        <div>
+          <div className={'app-container'}>
+            {postagem.nomeUsuario}
+          </div>
+          <div className={'app-container'}>
+             {postagem.fotoUsuario}
+          </div>
+          <div className={'app-container'}>
+             {postagem.fotoPost}
+          </div>
+        </div>  
+      );
+    });
     return (
-      <div>
-        <div className={'app-container'}>
-          <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-        </div>
-        <div className={'app-container'}>
-          <Post
-            nomeUsuario={'joana22'}
-            fotoUsuario={'https://picsum.photos/50/51'}
-            fotoPost={'https://picsum.photos/200/151'}
-          />
-        </div>
-        <div className={'app-container'}>
-          <Post
-            nomeUsuario={'a_malukinha'}
-            fotoUsuario={'https://picsum.photos/50/55'}
-            fotoPost={'https://picsum.photos/200/157'}
-          />
-        </div>
+      <div className={'app-container'}>
+        {listaDeComponentes}
       </div>
-    );
+
+    )
   }
 }
 
