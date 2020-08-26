3ae47871b3b0721b7eb81416ec71aa1e48d5e13f
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-26 17:37:00 -0300

    exercicios completos. Em andamento com os desafios

--- semana7/labenusers/labenusers/src/App.js
+++ semana7/labenusers/labenusers/src/App.js
@@ -3,78 +3,57 @@ import LogOn from './components/LogOn';
 import UsersList from './components/UsersList';
 import axios from "axios";
 import styled from "styled-components"
+import UserDetail from './components/UserDetail';
 
 const AppContainer = styled.main`
   display: flex;
-  
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default class App extends React.Component{
   state={
-    users: [],
-    inputName: "",
-    inputEmail: ""
+    showMe: "logOn",
+    editUser: false
   };
 
-  getUsers = () =>{
-    const request = axios.get(
-        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-        {
-          headers: {
-            Authorization: "claudia-trevisan-jackson"
-          }
-        }
-    );
 
-    request
-    .then((response) =>{
-        this.setState({users: response.data.name})
-    }).catch((error) =>{
-        alert("Ocorreu um erro")
-    });
-  };
-
-  logOnUsers = (name, email) =>{
-    const body = {
-      name: name,
-      email: email
-    };
-    const request = axios.post(
-      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-      body,
-      {
-        headers: {
-          Authorization: "claudia-trevisan-jackson"
-        }
-      }
-    );
-    request
-    .then((response) =>{
-      alert(`Usuário ${this.state.inputName} cadastrado com sucesso.`)
-      this.setState({inputName: ""});
-      this.setState({inputEmail: ""});
-    }).catch((error) =>{
-      alert("Ocorreu um erro")
-    })
-    
-  };
-
-  onChangeInput = (event) =>{
-    this.setState({inputName: event.target.value})
+  changePage = () =>{
+    if(this.state.showMe === "logOn"){
+      this.setState({showMe: "usersList"})
+    }else{
+      this.setState({showMe: "logOn"})
+    }
   };
   
-  onChangeInputEmail = (event) =>{
-      this.setState({inputEmail: event.target.value})
-  };
+  onClickEditUser = () =>{
+    this.setState({editUser: !this.state.editUser})
+    console.log(this.state.editUser)
 
+  }
 
   render() {
+    const currentPage = () =>{
+      if(this.state.showMe === "logOn"){
+        return <LogOn/>
+      }else if(this.state.showMe === "usersList"){
+        return <UsersList functionOnClick={this.onClickEditUser}/>
+      }
+    };
+
+    const editUsers = () =>{
+      if(this.state.editUser){
+        return <UserDetail />
+      }
+    }
 
     return (
       <AppContainer>
-        <LogOn inputName={this.state.inputName} inputEmail={this.state.inputEmail} pass={this.logOnUsers} again={this.onChangeInput} andAgain={this.onChangeInputEmail}/>
-        <UsersList passingList={this.state.users}/>
+        <button onClick={this.changePage}>Ir para página de lista</button>
+        {editUsers}
+        {currentPage()}
       </AppContainer>
     );
   }
-}
+};
--- semana7/labenusers/labenusers/src/components/LogOn.js
+++ semana7/labenusers/labenusers/src/components/LogOn.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import axios from "axios"
 import styled from "styled-components"
+import { axiosConfig, baseUrl } from './ConfigApi';
 
 const ContainerLogOn = styled.div`
     border: 1px solid;
     padding: 8px;
+    width: 60%;
 `;
 
 const Input = styled.div`
@@ -28,22 +30,52 @@ const Input = styled.div`
 `;
 
 export default class LogOn extends React.Component{
-
+    state={
+        inputName: "",
+        inputEmail: ""
+    };
     
+    onChangeName = (event) =>{
+        this.setState({inputName: event.target.value})
+      };
+      
+    onChangeEmail = (event) =>{
+        this.setState({inputEmail: event.target.value})
+    };
+    
+    logOnUsers = () =>{
+        const body = {
+          name: this.state.inputName,
+          email: this.state.inputEmail
+        };
+        const request = axios.post(
+          baseUrl,
+          body,
+          axiosConfig
+        );
+        request
+        .then((response) =>{
+          alert(`Usuário ${this.state.inputName} cadastrado com sucesso.`)
+          this.setState({inputName: "", inputEmail: ""});
+        }).catch((error) =>{
+          alert("Ocorreu um erro")
+        })
+        
+      };
 
     render(){
         return(
             <ContainerLogOn>
                 <Input>
                     <label>Nome:
-                        <input type="text" value={this.props.inputName} onChange={this.props.again}/>
+                        <input type="text" value={this.state.inputName} onChange={this.onChangeName}/>
                     </label>
                 </Input>
                 <Input>
                     <label>Email:
-                        <input type="email" value={this.props.inputEmail} onChange={this.props.andAgain}/>
+                        <input type="email" value={this.state.inputEmail} onChange={this.onChangeEmail}/>
                     </label>
-                    <button onClick={() => this.props.pass(this.props.inputName, this.props.inputEmail)}>Salvar</button>            
+                    <button onClick={this.logOnUsers}>Salvar</button>            
                 </Input>
             </ContainerLogOn>
         )
