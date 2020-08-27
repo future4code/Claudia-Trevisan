38c528a0fdc0e035b04c5c4f93413ef00c120567
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-27 16:56:25 -0300

    nem comecei

---
+++ semana7/apis-na-natureza/takeapi/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class App extends React.Component{
+  state={
+    seasons: "",
+    
+  }
+
+  render(){
+    return (
+      <div>
+        select com as temporadas por numero, 
+        ao selecionar aparece as infos da temporada(imagem, o nome das queens e a colocação de cada uma)
+        quando clica no nome da queen vai pegar 
+      </div>
+    );
+  };
