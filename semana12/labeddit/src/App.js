7ccd17b0717443a88050eae70143cc1e3e957f97
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-09-30 15:26:57 -0300

    inicio

---
+++ semana12/labeddit/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const url = "https://us-central1-labenu-apis.cloudfunctions.net/labEddit/login"
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
