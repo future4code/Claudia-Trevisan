173cf39a3feb4e7c3bdd071bc66c180075d666ff
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-10-19 17:50:48 -0300

    completos sem desafios

---
+++ semana15/back-aula1/node-package-template/exercicio-1/exercicio1.js
@@ -0,0 +1,9 @@
+// EXERCICIO 1
+// A) Devemos declara-los antes.
+
+// B) e C)
+const nam = process.argv[2]
+const age = Number(process.argv[3])
+const seven = Number(7)
+
+console.log('Olá, ', nam,'! Você tem ', age, ' anos. Em sete anos você terá',age+seven)
