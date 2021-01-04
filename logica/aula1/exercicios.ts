1cf2cfbb4074ccc52753d1157ad8df6824ebf1b6
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2021-01-04 16:41:04 -0300

    exercicio 1 feito

---
+++ logica/aula1/exercicios.ts
@@ -0,0 +1,16 @@
+const checkEdit = (firstWord: string, secondWord: string): boolean =>{
+    if (firstWord === secondWord) return false;
+    if (firstWord.length - secondWord.length > 1) return false;
+    if (firstWord.length > secondWord.length) return firstWord.includes(secondWord);
+    if (secondWord.length > firstWord.length) return secondWord.includes(firstWord);
+
+    let diferenceCharacter: number = 0
+    
+    for (let i = 0; i < firstWord.length; i++){
+        if (firstWord[i] !== secondWord[i]) diferenceCharacter ++
+    }
+
+    return diferenceCharacter === 1
+};
+
+console.log(checkEdit("bananaaa", "banana"))
